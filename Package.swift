8e68a253083d85120ddba91d515fc0bc4983519e
dariuszszlag <dariuszszlag@users.noreply.github.com>
2023-06-09 16:21:42 +0000

    Package.swift for v0.4.0

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "kbcore",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "kbcore",
+            targets: ["kbcore"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "kbcore",
+            path: "./kbcore.xcframework"
+        ),
+    ]
+)
